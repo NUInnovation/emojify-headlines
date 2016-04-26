87114c0762125a44034ffdacc91c1a42d0e5c043
Diane Liu <dianeliu@u.northwestern.edu>
2016-04-25 19:32:57 -0500

    basic emoji translaiton working

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, render_template
+import json
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+  return render_template('index.html')
+
+@app.route('/translate', methods=['POST'])
+def translate():
+  # get input text from form
+  inputtext = request.form['inputtext']
+
+  # load dictionary JSON object
+  dictionary = load_dictionary()
+  
+  # translate each word individually
+  translation = ""
+  for word in inputtext.split():
+    trans = dictionary_lookup(dictionary, word)
+    translation += trans
+
+  # render translation result
+  return render_template('index.html', originaltext=inputtext, translation=translation)
+
+def load_dictionary():
+  with open('emojis.json') as f:
+    dictionary = json.load(f)
+  return dictionary
+
+def dictionary_lookup(dictionary, word):
+  word = word.strip()
+  result = ""
+  result = unicode(result, 'utf-8')
+
+  for key, value in dictionary.iteritems():
+    if word == key:
+      result = value["char"]
+    else:
+      if word in value["keywords"]:
+        result = value['char']
+
+  return result
+
+if __name__ == '__main__':
+  app.debug = True
+  app.run()
