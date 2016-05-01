f0b3c8b32190415bc3058d57f46316571bcd4978
Diane Liu <dianeliu@u.northwestern.edu>
2016-05-01 17:02:04 -0500

    add nltk

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, render_template
 import json
+import nltk
+from nltk.corpus import wordnet as wn
+from nltk.stem.wordnet import WordNetLemmatizer
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,10 +16,11 @@ def translate():
 
   # load dictionary JSON object
   dictionary = load_dictionary()
-  
+
   # translate each word individually
   translation = ""
   for word in inputtext.split():
+    word = lemmatize(word)
     trans = dictionary_lookup(dictionary, word)
     translation += trans
 
@@ -28,9 +32,30 @@ def load_dictionary():
     dictionary = json.load(f)
   return dictionary
 
+def lemmatize(word):
+  # get part of speech of word
+  tagged = nltk.pos_tag([word])
+  tag = tagged[0][1][0:2]
+  print tag
+
+  if tag == "VB":
+    pos = wn.VERB
+  elif tag == "JJ":
+    pos = wn.ADJ
+  elif tag == "RB":
+    pos = wn.ADV
+  else:
+    pos = wn.NOUN
+
+  # lemmatize word
+  result = WordNetLemmatizer().lemmatize(word, pos)
+
+  print result
+  return result
+
 def dictionary_lookup(dictionary, word):
   # strip whitespace and make lowercase
-  word = word.rstrip(" !@#$%^&*()?.,").lower()
+  word = word.rstrip(" !@#$%^&*()?.,")
   result = ""
   result = unicode(result, 'utf-8')
 
