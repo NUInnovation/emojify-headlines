e16be0a4b5434c01b2a310510f952691908e5882
Diane Liu <dianeliu@u.northwestern.edu>
2016-05-02 22:31:45 -0500

    debug on

--- app.py
+++ app.py
@@ -76,6 +76,6 @@ def dictionary_lookup(dictionary, word):
   return result
 
 if __name__ == '__main__':
-  # app.debug = True
+  app.debug = True
   port = int(os.environ.get("PORT", 5000))
   app.run(host='0.0.0.0', port=port)
