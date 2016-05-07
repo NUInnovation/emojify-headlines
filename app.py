ac6b0d07911e368c372029fe5ed714e9d7d3dbbb
Diane Liu <dianeliu@u.northwestern.edu>
2016-05-07 15:39:08 -0500

    tokenize entire sentence instead of word

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import optparse
 import os
 import json
 import nltk
@@ -6,37 +7,56 @@ from nltk.corpus import wordnet as wn
 from nltk.stem.wordnet import WordNetLemmatizer
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
   return render_template('index.html')
 
-@app.route('/translate', methods=['POST'])
+@app.route('/translate', methods=['POST', 'GET'])
 def translate():
-  # get input text from form
-  inputtext = request.form['inputtext']
+  if request.method == 'POST':
+    global print_statements
 
-  # load dictionary JSON object
-  dictionary = load_dictionary()
+    # get input text from form
+    inputtext = request.form['inputtext']
 
-  # translate each word individually
-  translation = ""
-  for word in inputtext.split():
-    word = lemmatize(word)
-    trans = dictionary_lookup(dictionary, word)
-    translation += trans
+    # load dictionary JSON object
+    dictionary = load_dictionary()
 
-  # render translation result
-  return render_template('index.html', originaltext=inputtext, translation=translation)
+    # tokenize input sentence and POS tag
+    inputtext = inputtext.lower()
+    tokens = nltk.word_tokenize(inputtext)
+    tags = nltk.pos_tag(tokens) 
+    if print_statements:
+      print tags
+
+    # translate each word individually
+    translation = ""
+    for tag in tags:
+      if print_statements:
+        print tag[0]
+      word = lemmatize(tag[0], tag[1])
+      trans = dictionary_lookup(dictionary, word)
+      translation += trans
+
+    # render translation result
+    return render_template('index.html', originaltext=inputtext, translation=translation)
+  else:
+    return render_template('index.html')
 
 def load_dictionary():
   with open('emojis.json') as f:
     dictionary = json.load(f)
   return dictionary
 
-def lemmatize(word):
+def lemmatize(word, pos_tag):
+  global print_statements
+
   # get part of speech of word
-  tagged = nltk.pos_tag([word])
-  tag = tagged[0][1][0:2]
+  tag = pos_tag[0:2]
+
+  if print_statements:
+    print tag
 
   if tag == "VB":
     pos = wn.VERB
@@ -50,11 +70,13 @@ def lemmatize(word):
   # lemmatize word
   result = WordNetLemmatizer().lemmatize(word, pos)
 
+  if print_statements:
+    print result
+
   return result
 
 def dictionary_lookup(dictionary, word):
   # strip whitespace and make lowercase
-  word = word.rstrip(" !@#$%^&*()?.,").lower()
   result = ""
   result = unicode(result, 'utf-8')
 
@@ -71,10 +93,22 @@ def dictionary_lookup(dictionary, word):
   if result == None:
     result = ""
 
+  global print_statements
+  if print_statements:
+    print result
+
   return result
 
 if __name__ == '__main__':
   nltk.data.path.append('./nltk_data/')
+
+  parser = optparse.OptionParser()
+  parser.add_option("--print", action="store_true", dest="print_statements")
+  options, _ = parser.parse_args()
+
+  global print_statements
+  print_statements = options.print_statements
+
   app.debug = True
   port = int(os.environ.get("PORT", 5000))
   app.run(host='0.0.0.0', port=port)
